f7c53943377b541b748c63349a90605318587110
elgorhythm <adnan.ather@live.ca>
2023-02-26 19:49:07 -0700

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (<div className="navbar">
+    <div className="wrapper">
+        <div className="left">left</div>
+        <div className="center">center</div>
+        <div className="rightt">right</div>
+    </div>
+  </div>)
+};
+
+export default Navbar;
