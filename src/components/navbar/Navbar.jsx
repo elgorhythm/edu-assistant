c59f7d063eecac820d7c824eddd04c2da0c316a4
elgorhythm <adnan.ather@live.ca>
2023-02-26 20:59:12 -0700

    progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import "./navbar.scss";
+import alisLogo from '../../assets/alis-logo.png'
 
 const Navbar = () => {
   return (<div className="navbar">
     <div className="wrapper">
-        <div className="left">left</div>
-        <div className="center">center</div>
-        <div className="rightt">right</div>
+        <div className="left">
+          <img src={alisLogo} alt='Logo'/>
+        </div>
+        <div className="center">
+          <h3>
+            ALIS - Bucheki Branch
+          </h3>
+        </div>
+        <div className="right">
+          <h4>
+            welcome
+          </h4>
+        </div>
     </div>
   </div>)
 };
