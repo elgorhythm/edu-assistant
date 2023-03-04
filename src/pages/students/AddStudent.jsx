b04115f62bc02364f38f66870dea03566e79602f
AdnanAther <adnan.ather@live.ca>
2023-03-04 11:34:12 -0700

    progress

--- src/pages/students/AddStudent.jsx
+++ src/pages/students/AddStudent.jsx
@@ -1,5 +1,12 @@
 import "./students.scss";
-import { TextField } from "@mui/material";
+import {
+  TextField,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl,
+} from "@mui/material";
+import { classes } from "../../CONSTS";
 
 const AddStudent = () => {
   const totalCount = 101;
@@ -7,7 +14,7 @@ const AddStudent = () => {
   const handleSubmit = () => {};
   return (
     <div className="students">
-      <form form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <h3>New Admission</h3>
         <span> Details</span>
         <div className="input-wrappers">
@@ -17,6 +24,7 @@ const AddStudent = () => {
             required
             label="Full Name"
             name="name"
+            defaultValue=""
             className="text-field"
           />
           <TextField
@@ -25,6 +33,7 @@ const AddStudent = () => {
             required
             label="Father Name"
             name="father"
+            defaultValue=""
             className="text-field"
           />
         </div>
@@ -33,22 +42,154 @@ const AddStudent = () => {
             variant="standard"
             size="small"
             required
-            label="Registration No"
-            name="date"
+            label="Admission Date"
+            name="adDate"
             className="text-field"
-            value={`${new Date().getFullYear()}-${totalCount + 1}`}
+            type="date"
+            defaultValue=""
             InputLabelProps={{ shrink: true }}
-            disabled={true}
           />
           <TextField
             variant="standard"
             size="small"
             required
-            label="Admission Date"
-            name="registration"
+            label="Registration No"
+            name="regNo"
+            className="text-field"
+            defaultValue={`${new Date().getFullYear()}-${totalCount + 1}`}
+            InputLabelProps={{ shrink: true }}
+            disabled={true}
+          />
+        </div>
+        <div className="input-wrappers">
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            label="Date of Birth"
+            name="dob"
             className="text-field"
             type="date"
+            defaultValue=""
             InputLabelProps={{ shrink: true }}
+          />
+          <FormControl className="text-field">
+            <InputLabel>Gender *</InputLabel>
+            <Select
+              variant="standard"
+              size="small"
+              required
+              label="Gender"
+              name="gender"
+              defaultValue=""
+            >
+              <MenuItem value={"Male"}>Male</MenuItem>
+              <MenuItem value={"Female"}>Female</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="input-wrappers">
+          <FormControl className="text-field">
+            <InputLabel>Class *</InputLabel>
+            <Select
+              variant="standard"
+              size="small"
+              required
+              label="Class"
+              name="class"
+              defaultValue=""
+            >
+              {classes &&
+                classes.map((clas) => (
+                  <MenuItem value={clas} key={clas}>
+                    {clas}
+                  </MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+          <TextField
+            variant="standard"
+            size="small"
+            label="Section (optional)"
+            name="section"
+            className="text-field"
+          />
+        </div>
+        <div className="input-wrappers">
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            label="Address"
+            name="address"
+            className="text-field"
+          />
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            label="Contact No"
+            name="contact"
+            className="text-field"
+          />
+        </div>
+        <div className="input-wrappers">
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            label="Address"
+            name="address"
+            className="text-field"
+          />
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            label="Contact No"
+            name="contact"
+            className="text-field"
+          />
+        </div>
+        <div className="input-wrappers">
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            type="number"
+            label="Prospectus Fee"
+            name="prospectus"
+            className="text-field"
+          />
+          <TextField
+         variant="standard"
+         size="small"
+         required
+         type="number"
+         label="Registration Fee"
+         name="registration"
+         className="text-field"
+          />
+        </div>
+        <div className="input-wrappers">
+          <TextField
+            variant="standard"
+            size="small"
+            required
+            type="number"
+            label="Tuition Fee"
+            name="tuition"
+            className="text-field"
+          />
+          <TextField
+         variant="standard"
+         size="small"
+         required
+         type="number"
+         label="Arrears"
+         name="arrears"
+         className="text-field"
+         defaultValue={0}
           />
         </div>
       </form>
