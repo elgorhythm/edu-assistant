937a6372ec45bc49684b68a87e314033b9f177c5
elgorhythm <adnan.ather@live.ca>
2023-03-06 02:57:53 -0700

    id in studentsList

--- src/temp.js
+++ src/temp.js
@@ -88,108 +88,399 @@ export const monthlyIncome = {
 };
 export const monthlyExpenses = {
   202208: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
+
   202209: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
+
   202210: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
+
   202211: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
+
   202212: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
+
   202301: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
+
   202302: {
-  Miscellaneous: 5,
-  Staff: 6,
-  Utility: 7,
-  BooksPurchase: 8,
-  SchoolDevelopment: 9,
-  HeadOffice: 10,
-  ManagementFee: 11,
-  Dividend: 12,
-  Maintenance:14,
-  total: 25,
-  open: true,
-  canBeClosed: false,
+    Miscellaneous: 5,
+    Staff: 6,
+    Utility: 7,
+    BooksPurchase: 8,
+    SchoolDevelopment: 9,
+    HeadOffice: 10,
+    ManagementFee: 11,
+    Dividend: 12,
+    Maintenance: 14,
+    total: 25,
+    open: true,
+    canBeClosed: false,
   },
-  
 };
+
+export const studentsList=[{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:1
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:2
+  
+  
+  
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:3
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:4
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:5
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:6
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:7
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:8
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:9
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:10
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:11
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:12
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:13
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:14
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:15
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:16
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:17
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:18
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:19
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:20
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:21
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:22
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:23
+
+},
+{
+  name: "Student Student",
+  father: "Father Father",
+  regNo: "2023-205",
+  dateOfBirth: "2018-07-14",
+  gender: "Male",
+  class: "Class-1",
+  tuition: 4500,
+  contactNo: "03331234567",
+  id:24
+
+},
+]
