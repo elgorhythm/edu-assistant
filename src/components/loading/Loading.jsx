8a12439039c2dd65c9293527cb473e3808605d6d
Adnan <adnan.ather@live.ca>
2023-03-07 11:05:11 +0000

    progress

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,10 +1,26 @@
 import { CircularProgress } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 import "./loading.scss";
 const Loading = () => {
+  const [loading, setLoading] = useState("Loading");
+
+  useEffect(() => {
+    let x = 4;
+    const timer = setInterval(() => {
+      if (x % 4 === 0) setLoading("Loading");
+      if (x % 4 === 1) setLoading("Loading.");
+      if (x % 4 === 2) setLoading("Loading..");
+      if (x % 4 === 3) setLoading("Loading...");
+      x = x + 1;
+      console.log("settimeout running and x value is", x);
+    }, 700);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="loading">
       <CircularProgress className="icon" />
-      Loading...
+      {loading}
     </div>
   );
 };
