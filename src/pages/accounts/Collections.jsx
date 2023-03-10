f831bd694ccf3782bedc114eaf93ab8fcbc156ab
elgorhythm <adnan.ather@live.ca>
2023-03-09 17:37:37 -0700

    progress

--- src/pages/accounts/Collections.jsx
+++ src/pages/accounts/Collections.jsx
@@ -7,11 +7,14 @@ import {
   SearchOutlined,
   TuneOutlined,
 } from "@mui/icons-material";
-import CollectionCard from "./CollectionCard";
+import CollectionCard from "./components/CollectionCard";
+import { Radio, RadioGroup } from "@mui/material";
 
 const Collections = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [filterDisplay, setFilterDisplay]=useState(false)
+  const [filterDisplay, setFilterDisplay] = useState(false);
+  const [range, setRange] = useState("week");
+  console.log("range is ", range);
   return (
     <div className="collections">
       <div className="wrapper">
@@ -22,11 +25,36 @@ const Collections = () => {
           </div>
           <div className="title-wrapper">
             <h3>All Collections</h3>
-            <div className="filter" onClick={()=>setFilterDisplay(!filterDisplay)}>
-                <TuneOutlined/>
-                <div className="filter-selector" style={{display: filterDisplay? "flex":"none"}}>
-                    hi
-                </div>
+            <div className="filter">
+              <div
+                className="icon"
+                onClick={() => setFilterDisplay(!filterDisplay)}
+              >
+                <TuneOutlined sx={{ fontSize: "20px" }} />
+              </div>
+              <div
+                className="filter-selector"
+                style={{ display: filterDisplay ? "flex" : "none" }}
+              >
+                <RadioGroup
+                  row
+                  className="radio-group"
+                  defaultValue="male"
+                  onChange={(e) => console.log(e.target.value)}
+                  sx={{ display: "flex" }}
+                >
+                  asd
+                  <Radio label="hi" value="male" />
+                  <Radio value="female" />
+                </RadioGroup>
+                {/* <div onChange={(e) => console.log(e.target.value)}>
+                  <input type="radio" value="hi" checked={false} />
+                  <input type="radio" value="hola" checked={false} />
+                </div> */}
+                <button onClick={() => setFilterDisplay(!filterDisplay)}>
+                  x
+                </button>
+              </div>
             </div>
           </div>
           <div className="buttons-wrapper">
