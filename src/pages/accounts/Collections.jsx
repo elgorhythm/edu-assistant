5fd6c353b1057d601ad239312ca1dc1f7da339b3
elgorhythm <adnan.ather@live.ca>
2023-03-11 14:50:02 -0700

    progress

--- src/pages/accounts/Collections.jsx
+++ src/pages/accounts/Collections.jsx
@@ -1,111 +1,44 @@
 import "./accounts.scss";
 
 import React, { useState } from "react";
-import {
-  AddOutlined,
-  DoneOutlined,
-  PrintOutlined,
-  SearchOutlined,
-  TuneOutlined,
-} from "@mui/icons-material";
 import CollectionCard from "./components/CollectionCard";
-import { Radio, RadioGroup } from "@mui/material";
+import SearchContainer from "./components/SearchContainer";
+import { collectionsList } from "../../temp";
+import Loading from "../../components/loading/Loading";
 
 const Collections = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [filterDisplay, setFilterDisplay] = useState(false);
-  const [range, setRange] = useState("week");
+  const [searchBy, setSearchBy] = useState("studentName");
+  const [dateRange, setDateRange] = useState("week");
 
-  return (
+  // const collectionsList = null;
+
+  const getSearchTerm = (search) => {
+    setSearchTerm(search);
+  };
+  const getDateRange = (range) => {
+    setDateRange(range);
+  };
+
+  console.log(searchTerm);
+  console.log(dateRange);
+
+  return collectionsList ? (
     <div className="collections">
       <div className="wrapper">
-        <div className="top-container">
-          <div className="search-wrapper">
-            <SearchOutlined className="icon" />
-            <input onChange={(e) => setSearchTerm(e.target.value)} />
-          </div>
-          <div className="title-wrapper">
-            <h3>All Collections</h3>
-            <div className="filter">
-              <div
-                className="icon"
-                onClick={() => setFilterDisplay(!filterDisplay)}
-              >
-                <TuneOutlined sx={{ fontSize: "20px" }} />
-              </div>
-              <div
-                className="filter-selector"
-                style={{ display: filterDisplay ? "flex" : "none" }}
-              >
-                <h4>Select Range</h4>
-                <div className="radios-container">
-                  <div className="radio-wrapper">
-                    1 Week
-                    <input
-                      type="radio"
-                      value="week"
-                      checked={range === "week"}
-                      onChange={(e) => setRange(e.target.value)}
-                    />
-                  </div>
-                  <div className="radio-wrapper">
-                    1 Month
-                    <input
-                      type="radio"
-                      value="month"
-                      checked={range === "month"}
-                      onChange={(e) => setRange(e.target.value)}
-                    />
-                  </div>
-                  <div className="radio-wrapper">
-                    3 Months
-                    <input
-                      type="radio"
-                      value="threeMonths"
-                      checked={range === "threeMonths"}
-                      onChange={(e) => setRange(e.target.value)}
-                    />
-                  </div>
-                  <div className="radio-wrapper">
-                    {new Date().getFullYear()}
-                    <input
-                      type="radiO"
-                      value={`${new Date().getFullYear()}`}
-                      checked={range === `${new Date().getFullYear()}`}
-                      onChange={(e) => setRange(e.target.value)}
-                    />
-                  </div>
-                  <div className="radio-wrapper">
-                    {new Date().getFullYear() - 1}
-                    <input
-                      type="radiO"
-                      value={`${new Date().getFullYear() - 1}`}
-                      checked={range === `${new Date().getFullYear() - 1}`}
-                      onChange={(e) => setRange(e.target.value)}
-                    />
-                  </div>
-                </div>
-
-                <button onClick={() => setFilterDisplay(!filterDisplay)}>
-                  <DoneOutlined />
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="buttons-wrapper">
-            <button className="print">
-              <PrintOutlined />
-            </button>
-            <button className="add">
-              <AddOutlined />
-            </button>
-          </div>
-        </div>
-        <div className="collections-container">
-          <CollectionCard />
-        </div>
+        <SearchContainer
+          getSearchTerm={getSearchTerm}
+          getDateRange={getDateRange}
+        />
+        <CollectionCard
+          collectionsList={collectionsList}
+          searchTerm={searchTerm}
+          searchBy={searchBy}
+        />
       </div>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
--- src/pages/accounts/components/CollectionCard.jsx
+++ src/pages/accounts/components/CollectionCard.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
-const CollectionCard = () => {
+const CollectionCard = (props) => {
+  const { collectionsList, searchTerm, searchBy } = props;
   return (
-    <div>CollectionCard</div>
-  )
-}
+    <div className="collection-card">
+      <table>
+        <thead>
+          <tr>
+            <th id="sr">Sr.</th>
+            <th id="date">Date</th>
+            <th id="name">Student</th>
+            <th id="reg-no">Reg. No.</th>
+            <th id="category">Category</th>
+            <th id="month">Month</th>
+            <th id="amount">Amount</th>
+            <th id="amount">Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {collectionsList
+            .filter((income) =>
+              income[searchBy].toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((income, index) => (
+              <tr key={index}>
+                <td id="sr" className="sm">
+                  {index + 1}
+                </td>
+                <td id="date">{income.date}</td>
+                <td id="name">
+                  {income.category !== "Miscellaneous"
+                    ? income.studentName
+                    : "-"}
+                </td>
+                <td id="reg-no">
+                  {income.category !== "Miscellaneous"
+                    ? income.studentRegNo
+                    : "-"}
+                </td>
+                <td id="category">{income.category}</td>
+                <td id="month">
+                  {income.category === "Tuition"
+                    ? `${income.month}-${income.year}`
+                    : "-"}
+                </td>
+                <td id="amount">{income.amount}</td>
+                <td id="amount">
+                  {income.category !== "Miscellaneous" ? income.balance : "-"}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default CollectionCard
+export default CollectionCard;
